8aea484a7d69cff91c1152e7a1583527fcf8d6a7
learning-dev <ayazm.140895@gmail.com>
2020-01-09 20:52:22 +0530

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>Movies Inventory</h1>
+      <Link style={linkStyle} to="/movies">Movies</Link> | <Link style={linkStyle} to="/directors">Directors</Link> 
+    </header>
+  )
+}
+
+const headerStyle = {
+  background: '#0d0d0d',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px'
+}
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none',
+}
+
+export default Header;
